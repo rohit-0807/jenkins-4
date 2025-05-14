02a7222bb558e3b98f1c40d08d8a9dbdab7db49c
rohit-0807 <rohitjeerankalagi07@gmail.com>
2025-05-14 09:49:25 +0530

    save

---
+++ Demo.java
@@ -0,0 +1,10 @@
+
+
+public class Demo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int mutiply = a * b;
+        System.out.println("Multiplication of " + a + " and " + b + " is: " + mutiply);
+    }   
+}
